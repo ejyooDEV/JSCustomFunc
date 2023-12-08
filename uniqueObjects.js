2972f61bcb8c5c8eae900015d343360f58ba1b6c
Sally (YOOEUNJI) <89239387+ejyooDEV@users.noreply.github.com>
2023-12-08 18:27:12 +0900

    Create uniqueObjects.js
    Create uniqueObjects.js

---
+++ uniqueObjects.js
@@ -0,0 +1,18 @@
+/**
+ * array 객체에서 trackerTypeId를 기준으로 중복을 제거하는 함수
+ * @param {*} array 
+ * @returns 
+ */
+removeDuplicatesByArray(array, key) {
+    var uniqueObjects = [];
+    var uniqueIds = new Set();
+
+    for (var item of array) {
+        if (!uniqueIds.has(item[key])) {
+            uniqueIds.add(item.TrackerTypeId);
+            uniqueObjects.push(item);
+        }
+    }
+
+    return uniqueObjects;
+},
